f814626428a4a2dd08ac4213334fea210689615d
Pranjal Srivastava <75536136+iampsrv@users.noreply.github.com>
2023-07-09 12:07:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def delete_item_from_db(key):
     
 @app.route('/')
 def hello():
-    return "hello and welcome everyone leats learn about cicd"
+    return "hello and welcome everyone lets learn about cicd with jenkins"
     
 @app.route('/items', methods=['GET'])
 def get_all_items():
